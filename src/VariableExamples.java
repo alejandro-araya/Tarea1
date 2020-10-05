97b288be74f68f112b9f1f7958edef9202a1a31b
alejandro-araya <alejandro.araya@ulead.ac.cr>
2020-10-04 19:56:05 -0600

    Entrega Tarea 1

---
+++ src/VariableExamples.java
@@ -0,0 +1,14 @@
+public class VariableExamples {
+    public static void main(String[] args) {
+
+
+        short shortNumber = 1402;
+        long longNumber = 651456776000L;
+        double extendedNumber = 45696678615198.643;
+
+        System.out.println(shortNumber);
+        System.out.println(longNumber);
+        System.out.println(extendedNumber);
+
+    }
+}
